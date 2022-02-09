5870e2e343efee66328c2b7e15180a9fed987283
Youssef Adel <youssefelking2022@gmail.com>
2022-02-09 18:08:01 +0200

    First Assignment (attempt 1)

---
+++ flutter_application_1/lib/main.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Color.fromARGB(255, 147, 150, 149),
+          centerTitle: true,
+          actions: [
+            PopupMenuButton(
+                itemBuilder: ((context) =>
+                    [PopupMenuItem(child: Text("Youssef Created this"))]))
+          ],
+          title: Text(
+            "First Assignment",
+            style: TextStyle(
+                color: Color.fromARGB(115, 32, 11, 11),
+                fontSize: 25,
+                fontStyle: FontStyle.italic,
+                fontWeight: FontWeight.bold),
+          ),
+        ),
+        body: Center(
+            child: Container(
+          padding: EdgeInsets.only(left: 20.0),
+          height: 250,
+          width: 250,
+          alignment: Alignment.center,
+          margin: EdgeInsets.all(40),
+          decoration: BoxDecoration(
+              color: Colors.green,
+              border: Border.all(color: Colors.black, width: 40),
+              shape: BoxShape.circle),
+          child: Text(
+              "My favorite hobby is Swimming , love it because it gives me strong arms",
+              style: TextStyle(
+                color: Colors.orange,
+                fontSize: 10,
+              )),
+        ))),
+  ));
+}
